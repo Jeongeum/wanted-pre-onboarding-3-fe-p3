0b868b5d772fceb40689b92724dbcb65e0cebc26
Jeongeum <wjddma1202@naver.com>
2023-03-14 09:21:32 +0900

    세션기반 로그인 호출

--- client/src/api/login.js
+++ client/src/api/login.js
@@ -1,17 +1,34 @@
-import { BASE_URL } from './const.js'
+import { BASE_URL } from "./const.js";
 
 export const login = async (args) => {
   // TODO 3-1: POST, '/auth/login' 호출
   // body에는 { username, password }가 들어가야 함
   // 사용하는 기술에 맞추어 적절히 withCredential 설정하기
 
-  return 'fail'
-}
+  const loginRes = await fetch(`${BASE_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      credentials: "include", // <- 중요! 세션 방식 로그인을 위해 꼭 설정해 주세요.
+    },
+    body: JSON.stringify(args),
+  });
+
+  return loginRes.ok ? "success" : "fail";
+};
 
 export const getCurrentUserInfo = async () => {
   // TODO 3-2: GET, '/profile' 호출
   // 호출 성공시 유저 정보 반환
   // 마찬가지로 사용하는 기술에 맞추어 적절히 withCredential 설정하기
 
-  return null
-}
+  const userInfoRes = await fetch(`${BASE_URL}/profile`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      credentials: "include",
+    },
+  });
+
+  return userInfoRes ? userInfoRes.json() : null;
+};
--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from 'react-router-dom'
-import GeneralLayout from './layout/GeneralLayout.jsx'
-import Home from './pages/Home.jsx'
-import Login from './pages/Login.jsx'
-import PageA from './pages/PageA.jsx'
-import PageB from './pages/PageB.jsx'
-import PageC from './pages/PageC.jsx'
+import { createBrowserRouter, Router } from "react-router-dom";
+import GeneralLayout from "./layout/GeneralLayout.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import PageA from "./pages/PageA.jsx";
+import PageB from "./pages/PageB.jsx";
+import PageC from "./pages/PageC.jsx";
 
 // interface RouterElement {
 //   id: number // 페이지 아이디 (반복문용 고유값)
@@ -16,25 +16,79 @@ import PageC from './pages/PageC.jsx'
 
 const routerData = [
   // TODO 3-1: 로그인 페이지 라우터 등록하기 ('login', withAuth: false)
+
   // TODO 3-2: page a, b, c 등록하기
   {
     id: 0,
-    path: '/',
-    label: 'Home',
+    path: "/",
+    label: "Home",
     element: <Home />,
     withAuth: false,
-  }
-]
+  },
+
+  //todo 3-1 부분
+  {
+    id: 1,
+    path: "/login",
+    label: "로그인",
+    element: <Login />,
+    withAuth: false,
+  },
+
+  // Todo 3-2 부분 페이지 추가하기
+  {
+    id: 2,
+    path: "/page-a",
+    label: "페이지a",
+    element: <PageA />,
+    withAuth: false,
+  },
+
+  {
+    id: 3,
+    path: "/page-b",
+    label: "페이지b",
+    element: <PageB />,
+    withAuth: false,
+  },
+
+  {
+    id: 4,
+    path: "/page-c",
+    label: "페이지c",
+    element: <PageC />,
+    withAuth: false,
+  },
+];
 
 // TODO 3-1: 인증이 필요한 페이지는 GeneralLayout으로 감싸서 라우터에 전달
 // GeneralLayou에는 페이지 컴포넌트를 children으로 전달
-export const routers = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  }
-])
+export const routers = createBrowserRouter(
+  routerData.map((router) => {
+    if (router.withAuth) {
+      return {
+        path: router.path,
+        element: <GeneralLayout>{router.element}</GeneralLayout>,
+      };
+    } else {
+      return {
+        path: router.path,
+        element: router.element,
+      };
+    }
+  })
+);
 
 // TODO 3-2: 라우터 객체에서 인증이 필요한 페이지만 필터링해 사이드바에 전달
 // id, path, label을 전달하여 Sidebar에서 사용
-export const SidebarContent = []
+export const SidebarContent = routerData.reduce((prev, router) => {
+  if (!router.withAuth) return prev;
+  return [
+    ...prev,
+    {
+      id: router.id,
+      path: router.path,
+      label: router.label,
+    },
+  ];
+}, []);
